db918e88bf67c56f2f909dafa1af701e3dc170fe
Julia Ramm <juliaramm.dev@gmail.com>
2025-12-06 12:26:03 -0300

    atualização
    Updated comments for clarity and consistency.

--- livraria/rodando.py
+++ livraria/rodando.py
@@ -1,11 +1,12 @@
 from catalogo_livros import exibir_catalogo, catalogo_padrao
 from adicionando_carrinho import adicionar_livro
 
-# pra mostrar o catálogo
+# mostrar o catálogo
 exibir_catalogo(catalogo_padrao)
 
-# cria carrinho vazio
+# criando o carrinho
 carrinho = []
 
-# função pra adicionar os livros
+# adicionando os livros
 adicionar_livro(carrinho)
+
